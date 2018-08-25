1e9de0416b590d816b882eadb613a02731e8e4bb
Pablo Vargas <pablovargasjr@gmail.com>
2018-08-25 15:27:19 -0400

    add map drawing tools, and add styling to the site

--- static/scripts/maps.js
+++ static/scripts/maps.js
@@ -1,15 +1,17 @@
-let map;
+var map;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 42.3601, lng: -71.0589}, zoom: 12
+        center: {lat: 42.3601, lng: -71.0589},
+        zoom: 12,
+        streetViewControl: false,        
     });
     
     let input = document.getElementById('pac-input');
     let options = {
         componentRestrictions: {country: "us"} // restrict search to the US
     };
-    //map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input);
+    
     let autocomplete = new google.maps.places.Autocomplete(input, options); 
 
     /////////////////////////////////////////////
@@ -19,11 +21,11 @@ function initMap() {
     autocomplete.setFields(
         ['address_components', 'geometry', 'icon', 'name']);
 
-    var infowindow = new google.maps.InfoWindow();
-    var infowindowContent = document.getElementById('infowindow-content');
+    let infowindow = new google.maps.InfoWindow();
+    let infowindowContent = document.getElementById('infowindow-content');
     infowindow.setContent(infowindowContent);
 
-    var marker = new google.maps.Marker({
+    let marker = new google.maps.Marker({
         map: map,
         anchorPoint: new google.maps.Point(0, -29)
       });
@@ -31,7 +33,7 @@ function initMap() {
     autocomplete.addListener('place_changed', function() {
         infowindow.close();
         marker.setVisible(false);
-        var place = autocomplete.getPlace();
+        let place = autocomplete.getPlace();
         if (!place.geometry) {
             // User entered the name of a Place that was not suggested and
             // pressed the Enter key, or the Place Details request failed.
@@ -39,7 +41,7 @@ function initMap() {
             return;
         }
 
-        // If the place has a geometry, then present it on a map.
+        // If the place has a geometry, then present it on map.
         if (place.geometry.viewport) {
             map.fitBounds(place.geometry.viewport);
         } else {
@@ -49,23 +51,47 @@ function initMap() {
         marker.setPosition(place.geometry.location);
         marker.setVisible(true);
 
-        var address = '';
+        // get address from location
+        let address = '';
         if (place.address_components) {
             address = [
             (place.address_components[0] && place.address_components[0].short_name || ''),
             (place.address_components[1] && place.address_components[1].short_name || ''),
             (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-        }
+            ].join(' ');  
 
+            let coords = place.geometry.location.lat() + ", " +         
+                place.geometry.location.lng();
+            
+            console.log(coords);
+        }
         
+        // Places informaiton box above marker
         infowindowContent.children['place-icon'].src = place.icon;
         infowindowContent.children['place-name'].textContent = place.name;
-        infowindowContent.children['place-address'].textContent = address;
-        
+        infowindowContent.children['place-address'].textContent = address;        
         infowindow.open(map, marker);
     });
-   // google.maps.event.addDomListener(window, 'load', initialize); 
+    let drawingShapeOtions = {
+        fillColor: '#fdc029',
+        fillOpacity: 0.5,
+        strokeWeight: 2,
+        clickable: false,
+        editable: true,
+        zIndex: 1
+    };
+    let drawingManager = new google.maps.drawing.DrawingManager({
+        
+       
+        drawingControl: true,
+        drawingControlOptions: {
+            position: google.maps.ControlPosition.TOP_CENTER,
+            drawingModes: ['polygon', 'rectangle']
+        },        
+        polygonOptions: drawingShapeOtions,
+        rectangleOptions: drawingShapeOtions
+    });
+    drawingManager.setMap(map);
 };
 
 
